dee72707e232ffe4b8b7335e07de01f575fde059
abhishekkjainn <jainabhishek1904@gmail.com>
2025-05-24 16:31:11 +0530

    Header Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="header">
+        <div className="greeting">
+          <div className="greetingtop">Welcome</div>
+          <div className="greetingbottom">Abhishek Jain</div>
+        </div>
+        <div className="menu">
+          <img src="/menu.png" alt="" className="menuicon" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
